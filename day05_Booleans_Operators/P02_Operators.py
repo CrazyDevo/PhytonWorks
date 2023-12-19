1d67e3e2a8cfd249116296d7daf2b9a2edeee820
AdamB25 <barsialtunit@gmail.com>
2023-12-19 12:06:41 -0500

    Operators done

---
+++ day05_Booleans_Operators/P02_Operators.py
@@ -0,0 +1,31 @@
+# In the example below, we use the + operator to add together two values:
+
+print(10 + 5)
+
+"""
+Python divides the operators in the following groups:
+
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators
+"""
+
+x = 3
+y = 3
+
+t=x**y
+print(t)  # 27
+
+print(5/2)  # 2.5
+
+print(5//2)  # 2
+a=5
+a ^= 3
+print(a)
+
+
+print(5*2/2)
